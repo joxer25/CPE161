995f0daaad16c9eafa806ecdf08b1bef0c9d0c47
joxer25 <118364296+joxer25@users.noreply.github.com>
2022-11-16 21:15:03 +0700

    Add files via upload

---
+++ a-z.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>	 
+int num;
+float y;
+char n;
+main()	 
+{	 
+     printf("Enter number : ");
+     scanf("%d",&num);	 
+     printf("Enter name : ");	 
+     scanf("%f",&n);	 
+     printf("Thank you");	 
+}
